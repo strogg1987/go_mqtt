2e0e02fa54c9c19447551a5384c20a77e1b6e36a
674xu8 <enginseer@enginseer.ru>
2022-03-28 01:48:01 +0700

    init

---
+++ go.mod
@@ -0,0 +1,15 @@
+module mqtt_kb
+
+go 1.18
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/mattn/go-tty v0.0.4
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/mattn/go-tty"
+)
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("go_test_client")
+	opts.SetKeepAlive(60 * time.Second)
+	// Set the message callback handler
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	// Subscribe to a topic
+	if token := c.Subscribe("testtopic/#", 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+	tty, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tty.Close()
+	for {
+		r, err := tty.ReadRune()
+		if err != nil {
+			log.Fatal(err)
+		}
+		//send message on key "z"
+		if string(r) == "z" {
+			token := c.Publish("testtopic/1", 0, false, "Hello World")
+			token.Wait()
+		}
+	}
+	// Disconnect
+	c.Disconnect(250)
+	time.Sleep(1 * time.Second)
+}
